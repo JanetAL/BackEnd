11649a322ce3247987ff5f8310c35c2d3d65b0e7
JanetAL <janet.ledesma26@gmail.com>
2023-02-23 18:38:21 -0300

    Instalacion y creacion de estructura del backend

---
+++ src/main/java/com/trabajofinalAP/JanetL/JanetLApplication.java
@@ -0,0 +1,13 @@
+package com.trabajofinalAP.JanetL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanetLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JanetLApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trabajofinalAP/JanetL/JanetLApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabajofinalAP.JanetL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JanetLApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
